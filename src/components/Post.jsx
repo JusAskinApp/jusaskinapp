762f470115e5751d163c80944fe260290319507b
umar khan <umerk7222@gmail.com>
2023-02-19 11:00:19 +0500

    scroll and field added

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -21,6 +21,7 @@ import { useState } from "react";
 
 const Post = (props) => {
   const [comment, setComment] = useState("");
+  const [comments, setComments] = useState("");
   const [localComments,setLocalComment] = useState([])
   const postComment = async (blogRefId) => {
     debugger;
@@ -61,6 +62,18 @@ const Post = (props) => {
     } else {
       return "0";
     }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const newComment = {
+      name: formData.get('name'),
+      comment: formData.get('comment'),
+      timestamp: new Date().toLocaleString(),
+    };
+    setComments((prevComments) => [...prevComments, newComment]);
+    event.target.reset();
   };
   
 
@@ -131,50 +144,67 @@ const Post = (props) => {
         </Typography>
       </Box>
       {props.comments.length > 0 ? (
-        <>
+        <div className="m-5 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
           {props.comments.map((item, index) => (
-             <form className="flex items-center p-5">
-             <EmojiHappyIcon className="h-7" />
-             <input
-               type="text"
-               value={item.comment}
-               disabled
-               className="border-none flex-1 focus:ring-0 outline-none mx-3"
-             />        
-           </form>
+            
+             <div className="flex items-center space-x-2 mb-3">
+             <img
+             className="h-7 rounded-full"
+             src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/462.jpg"
+             alt=""
+             />
+             <p className="text-sm flex-1">
+               <span className="font-bold">Abdul Haseeb</span>{" "}
+               <span>{item.comment}</span>
+               
+             </p>
+             <span className="text-xs pr-5">9 seconds ago</span>
+             
+           </div>
           ))}
-        </>
+        </div>
       ) : '' }
       {localComments.length > 0 ? (
-        <>
+        <div className="m-5 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
           {localComments.map((item, index) => (
-             <form className="flex items-center p-5">
-             <EmojiHappyIcon className="h-7" />
-             <input
-               type="text"
-               value={item.comment}
-               disabled
-               className="border-none flex-1 focus:ring-0 outline-none mx-3"
-             />        
-           </form>
+            <div className="flex items-center space-x-2 mb-3">
+            <img
+            className="h-7 rounded-full"
+            src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/462.jpg"
+            alt=""
+            />
+            <p className="text-sm flex-1">
+              <span className="font-bold">Abdul Haseeb</span>{" "}
+              <span>{item.comment}</span>
+              
+            </p>
+            <span className="text-xs pr-5">9 seconds ago</span>
+            
+          </div>
           ))}
-        </>
+        </div>
       ) : '' }
      
       {/* comments */}
 
-      <div className="ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
+      {/* <div className="m-5 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
         <div className="flex items-center space-x-2 mb-3">
           <img
           className="h-7 rounded-full"
           src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/462.jpg"
           alt=""
           />
+          <p className="text-sm flex-1">
+            <span className="font-bold">Abdul Haseeb</span>{" "}
+            <span>this is a static comment</span>
+            
+          </p>
+          <span className="text-xs pr-5">9 seconds ago</span>
           
         </div>
-      </div>
+      </div> */}
 
-      <form className="flex items-center p-5">
+      <form className="flex items-center p-5" onSubmit={handleSubmit}>
         <EmojiHappyIcon className="h-7" />
 
         <input
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -4,6 +4,7 @@ import SuccessAlert from "../components/SuccessAlert";
 import ErrorAlerts from "../components/ErrorAlerts";
 const Subscribe = () => {
   const [email, setEmail] = useState("");
+  const [interest, setInterest] = useState("");
   const [showSuccessAlert, setSuccessShowAlert] = useState(false);
   const [showErrorAlert, setErrorShowAlert] = useState(false);
   const [error, setError] = useState(false);
@@ -19,7 +20,7 @@ const Subscribe = () => {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            email: email
+            email: email,
           }),
         }
       )
@@ -33,14 +34,13 @@ const Subscribe = () => {
               window.location.reload();
             }, 2000);
           } else if (data == "You are already subciber") {
-            setError("You are already our subscriber")
+            setError("You are already our subscriber");
             setErrorShowAlert(true);
             setTimeout(() => {
               window.location.reload();
             }, 2000);
-          }
-          else {
-            setError("Please check your email or network")
+          } else {
+            setError("Please check your email or network");
             setErrorShowAlert(true);
             setTimeout(() => {
               window.location.reload();
@@ -65,24 +65,43 @@ const Subscribe = () => {
       {showSuccessAlert && <SuccessAlert />}
       {showErrorAlert && <ErrorAlerts error={error} />}
 
-      <h2 className="text-5xl font-bold mb-6 pt-20">Learn, Share, Connect</h2>
-      <form onSubmit={handleSubmit} className="mx-auto my-6">
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter your email address"
-          className="px-3 py-2 rounded-lg w-64 mb-6"
-        />
-        <button
-          className="bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600"
-          onClick={() => {
-            sendEmail();
-          }}
-        >
-          Subscribe
-        </button>
-      </form>
+      <h2 className="text-5xl font-bold mb-6 pt-20 lg:text-6xl">Learn, Share, Connect</h2>
+      <form onSubmit={handleSubmit} className="mx-auto my-8 flex flex-col sm:flex-row sm:justify-center sm:items-center">
+  <div className="ml-2 mb-6 sm:mb-0">
+    <input
+      type="email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      placeholder="Enter your email address"
+      className="px-3 py-2 rounded-lg w-64"
+    />
+  </div>
+
+  <div className="ml-2 mb-6 sm:mb-0">
+    <select
+      id="interest-select"
+      value={interest}
+      onChange={(e) => setInterest(e.target.value)}
+      className="px-3 py-2 rounded-lg w-64"
+    >
+      <option value="" disabled selected>Select your interest</option>
+      <option value="Technology">Technology</option>
+      <option value="Business">Business</option>
+      <option value="Health">Health</option>
+    </select>
+  </div>
+  <div className="mb-6 ml-2 sm:mb-0">
+  <button
+    className="bg-green-500 text-white px-3 py-2 rounded-lg hover:bg-green-600"
+    onClick={() => {
+      sendEmail();
+    }}
+  >
+    Subscribe
+  </button>
+  </div>
+</form>
+
       <img src={myImage} alt="image" className="mx-auto" />
     </div>
   );
